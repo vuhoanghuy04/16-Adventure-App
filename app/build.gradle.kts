e5c88d5d4ec642900d61c109a8bb9a3c2300220a
Huy Vu <vuhoanghuy04@icloud.com>
2026-05-21 22:44:16 +0700

    Bảo mật thông tin nhạy cảm và chuẩn bị merge lên master

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "AIzaSyARdPDDkD_muMH7glhayKil84kdHFDmlOw", "\"${project.findProperty("AIzaSyARdPDDkD_muMH7glhayKil84kdHFDmlOw") ?: ""}\"")
         }
         debug {
-            buildConfigField("String", "AIzaSyARdPDDkD_muMH7glhayKil84kdHFDmlOw", "\"${project.findProperty("AIzaSyARdPDDkD_muMH7glhayKil84kdHFDmlOw") ?: ""}\"")
         }
     }
     compileOptions {
